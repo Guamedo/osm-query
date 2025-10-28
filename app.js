dac1524db6b7378f8f56c0a874cf682aecbcaec5
Gonzalo Piérola Azanza <gpierola@vicomtech.org>
2025-10-28 09:32:17 +0100

    Refactor distance calculation and update function parameters for consistency

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function getDistanceInMetres(lat1, lon1, lat2, lon2) {
   return d;
 }
 
-function getElementsData(elements, centerLat, centerLon) {
+function getElementsData(elements, center) {
   const elementsData = elements
     .filter((e) => e.type === 'node' || e.type === 'way')
     .map((e) => {
@@ -36,7 +36,7 @@ function getElementsData(elements, centerLat, centerLon) {
           lat: e.lat,
           lon: e.lon,
           tags: e.tags,
-          distToCenter: getDistanceInMetres(e.lat, e.lon, centerLat, centerLon),
+          distToCenter: getDistanceInMetres(e.lat, e.lon, center.lat, center.lng),
         };
       } else {
         const wayLat = (e.bounds.minlat + e.bounds.maxlat) / 2;
@@ -45,20 +45,20 @@ function getElementsData(elements, centerLat, centerLon) {
           lat: wayLat,
           lon: wayLon,
           tags: e.tags,
-          distToCenter: getDistanceInMetres(wayLat, wayLon, centerLat, centerLon),
+          distToCenter: getDistanceInMetres(wayLat, wayLon, center.lat, center.lng),
         };
       }
     });
   return elementsData;
 }
 
-async function queryOverpass(query, centerLat, centerLon) {
+async function queryOverpass(query, center) {
   const result = await fetch('https://overpass-api.de/api/interpreter', {
     method: 'POST',
     body: 'data=' + encodeURIComponent(query),
   }).then((data) => data.json());
 
-  let elementsData = getElementsData(result?.elements ?? [], centerLat, centerLon);
+  let elementsData = getElementsData(result?.elements ?? [], center);
 
   elementsData.sort((a, b) => {
     return a.distToCenter - b.distToCenter;
@@ -67,48 +67,29 @@ async function queryOverpass(query, centerLat, centerLon) {
   return elementsData;
 }
 
-async function getPublicRestroomFromPos(lat, lon, dist = 20) {
-  const centerLat = lat;
-  const centerLon = lon;
+async function getPublicRestroomFromBounds(bounds) {
+  const southWest = bounds.getSouthWest();
+  const northEast = bounds.getNorthEast();
+  const center = bounds.getCenter();
 
-  const distKm = dist;
+  const bbox = `${southWest.lat},${southWest.lng},${northEast.lat},${northEast.lng}`;
 
-  const distLat = distKm / 111.111;
-  const distLon = distKm / (111.111 * Math.cos(centerLat * (Math.PI / 180)));
-
-  const bbox = `${centerLat - distLat / 2},${centerLon - distLon / 2},${centerLat + distLat / 2},${
-    centerLon + distLon / 2
-  }`;
-
-  return await queryOverpass(
-    `[out:json][timeout:120];nwr["amenity"="toilets"](${bbox});out geom;`,
-    centerLat,
-    centerLon
-  );
+  return await queryOverpass(`[out:json][timeout:120];nwr["amenity"="toilets"](${bbox});out geom;`, center);
 }
 
-async function getDrinkingWaterFromPos(lat, lon, dist = 10) {
-  const centerLat = lat;
-  const centerLon = lon;
+async function getDrinkingWaterFromBounds(bounds) {
+  const southWest = bounds.getSouthWest();
+  const northEast = bounds.getNorthEast();
+  const center = bounds.getCenter();
 
-  const distKm = dist;
+  const bbox = `${southWest.lat},${southWest.lng},${northEast.lat},${northEast.lng}`;
 
-  const distLat = distKm / 111.111;
-  const distLon = distKm / (111.111 * Math.cos(centerLat * (Math.PI / 180)));
-
-  const bbox = `${centerLat - distLat / 2},${centerLon - distLon / 2},${centerLat + distLat / 2},${
-    centerLon + distLon / 2
-  }`;
-
-  return await queryOverpass(
-    `[out:json][timeout:120];nwr["amenity"="drinking_water"](${bbox});out geom;`,
-    centerLat,
-    centerLon
-  );
+  return await queryOverpass(`[out:json][timeout:120];nwr["amenity"="drinking_water"](${bbox});out geom;`, center);
 }
 
 async function main() {
   const map = L.map('map').setView([43.264331, -2.9207012], 14);
+  let centerMarker = null;
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -118,58 +99,61 @@ async function main() {
   const findBtn = document.getElementById('find-btn');
   findBtn.addEventListener('click', find);
 
-  async function find() {
-    map.eachLayer((layer) => {
-      if (!layer._url) layer.remove();
-    });
+  const locateBtn = document.getElementById('locate-btn');
+  locateBtn.addEventListener('click', locate);
 
+  async function locate() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
         async (position) => {
-          findBtn.innerHTML = 'Finding...';
-
           const lat = position?.coords?.latitude;
           const lon = position?.coords?.longitude;
 
-          L.marker([lat, lon], { icon: redIcon }).addTo(map);
+          if (centerMarker) centerMarker.remove();
+          centerMarker = L.marker([lat, lon], { icon: redIcon }).addTo(map);
 
           map.setView([lat, lon], 14);
-
-          const findSelection = document.querySelector('input[name="search-tag"]:checked').value;
-
-          try {
-            const nodes =
-              findSelection === 'toilet'
-                ? await getPublicRestroomFromPos(lat, lon)
-                : await getDrinkingWaterFromPos(lat, lon);
-            for (const node of nodes) {
-              const m = L.marker([node.lat, node.lon]).addTo(map);
-              const popupBody = `
-                <p style="margin: 0"> Distance: ${(node.distToCenter / 1000).toFixed(3) + ' Km'} </p> 
-                <a href="https://www.google.com.sa/maps/search/${node.lat},${node.lon}" target="_blank">Navigate</a>
-              `;
-              m.bindPopup(popupBody);
-            }
-          } catch (err) {
-            console.error(err);
-
-            console.warn('Error finding');
-          } finally {
-            findBtn.innerHTML = 'Find';
-          }
         },
         (error) => {
-          const errors = {
-            1: 'Permission denied',
-            2: 'Position unavailable',
-            3: 'Request timeout',
-          };
+          const errors = { 1: 'Permission denied', 2: 'Position unavailable', 3: 'Request timeout' };
           alert('Error: ' + errors[error.code]);
         },
         { enableHighAccuracy: true }
       );
     } else {
       console.warn('Geolocation is not available in this browser');
+    }
+  }
+
+  async function find() {
+    map.eachLayer((layer) => {
+      if (!layer._url && layer != centerMarker) layer.remove();
+    });
+
+    findBtn.innerHTML = 'Finding...';
+
+    const bounds = map.getBounds();
+    const findSelection = document.querySelector('input[name="search-tag"]:checked').value;
+
+    try {
+      const nodes =
+        findSelection === 'toilet'
+          ? await getPublicRestroomFromBounds(bounds)
+          : await getDrinkingWaterFromBounds(bounds);
+      for (const node of nodes) {
+        const m = L.marker([node.lat, node.lon]).addTo(map);
+        const popupBody = `
+          <p style="margin: 0"> Distance: ${(node.distToCenter / 1000).toFixed(3) + ' Km'} </p> 
+          <a href="https://www.google.com.sa/maps/search/${node.lat},${node.lon}" target="_blank">Navigate</a>
+        `;
+        m.bindPopup(popupBody);
+      }
+    } catch (err) {
+      console.error(err);
+
+      console.warn('Error finding');
+    } finally {
+      findBtn.innerHTML = 'Find';
     }
   }
 }
